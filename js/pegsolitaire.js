f8d55d68a5cd293f8369b5febfe0bea07c15516c
Sameer Marathe <transmogrifier@gmail.com>
2017-07-26 19:24:25 -0400

    First working prototype. Still need to resolve some issues.

--- js/pegsolitaire.js
+++ js/pegsolitaire.js
@@ -1,32 +1,40 @@
 //Peg, has 2 coordinates and a state
-function Peg(row, col, isEmpty){
+function Peg(row, col, id, index, isEmpty){
   this.isEmpty = isEmpty;
   this.row = row;
   this.col = col;
+  this.id = id;
+  this.index = index;
 }
 
+//Game board
 function Board(){
     this.boardType = 'Triangle';
     this.numPegs = 0;
     this.pegs = this.getPegs();
     this.html = this.make_html();
+    this.moveFrom = -1;
+    this.moveTo = -1;
 }
 
 Board.prototype = {
     getPegs: function(){
         pegs = [];
+        var alphabet = 'abcdefghijklmnopqrstuvwxyz'
         switch(this.boardType){
             case 'Triangle':
             default:
                 //Defines a triangular board with 5 pegs per side
                 for(i = 0; i < 5; i++){
                   for(j = 0; j <= i; j++){
+                    var id = alphabet[j] + i.toString();
+                    var index = (i*(i + 1)/2) + j;
                     if (i === 0 && j === 0){
                         //Set top corner as empty
-                        pegs.push(new Peg(i,j,true));
+                        pegs.push(new Peg(i, j, id, index, true));
                     } else{
                         //Set all other pegs as occupied
-                        pegs.push(new Peg(i,j,false));
+                        pegs.push(new Peg(i, j, id, index, false));
                     }
                     this.numPegs++;
                   }
@@ -47,7 +55,7 @@ Board.prototype = {
         switch(this.boardType){
             case 'Triangle':
             default:
-                var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+                // var alphabet = 'abcdefghijklmnopqrstuvwxyz'
                 var pitchX = 40;
                 var pitchY = 25;
                 var radius = 10;
@@ -58,76 +66,72 @@ Board.prototype = {
                   var cx = left + pitchX*curPeg.col;
                   var curCircle = $('<circle></circle>');
                   var pegState = curPeg.isEmpty ? 'empty':'occupied';
-                  var pegId = alphabet[curPeg.col] + curPeg.row.toString();
+                  var pegId = curPeg.id;
                   var txtId = pegId +'-txt';
-                  svg.circle(cx,cy,radius,{id:pegId, state:pegState});
+                  var pegIndex = curPeg.index;
+                  svg.circle(cx,cy,radius,
+                             {id:pegId, index: pegIndex, state:pegState});
                   svg.text(cx,cy,pegId,
-                           {id:txtId, state:pegState, stroke:'white', textAnchor:'middle'});
+                           {id:txtId, index: pegIndex, state:pegState,
+                            stroke:'white', textAnchor:'middle'});
                 }
                 break;
         }
+        var movesLog = $("<p></p>")
+        movesLog.attr("id", "move-log");
+        movesLog.text("Moves:");
+        gameBoard.append(movesLog);
         return gameBoard;
     },
 
-    make_a_move: function(move){
-        var result = "err: ";
-        var alphabet = "abcdefghijklmnopqrstuvwxyz";
-        if ((typeof move) === "string"){
-            var mPegs = move.split("-");
-            var p1 = mPegs.shift().trim();
-            p1 = p1.toLowerCase();
-            while (mPegs.length > 0){
-                var p2 = mPegs.shit().trim();
-                p2 = p2.toLowerCase();
-                var nP1 = get_peg_index(p1);
-                var nP2 = get_peg_index(p2);
-                var peg1 = this.pegs[nP1];
-                var peg2 = this.pegs[nP2];
-                if (!peg1.isEmpty && peg2.isEmpty){
-                    //Check if a valid move can be made
-                    var iRow = -1;
-                    var iCol = -1;
-                    // if ((peg1.row === peg2.row) &&
-                    //     (Math.abs(peg1.col - peg2.col) === 2)){
-                    //     iRow = peg1.row;
-                    //     iCol = (peg1.col + peg2.col)/2;
-                    // } else if ((peg1.col === peg2.col) &&
-                    //            (Math.abs(peg1.row - peg2.row) === 2)){
-                    //     iRow = (peg1.row + peg2.row)/2;
-                    //     iCol = peg1.col;
-                    // } else if ((Math.abs(peg1.col - peg2.col) === 2) &&
-                    //            (Math.abs(peg1.row - peg2.row) === 2)){
-                    //     iRow = (peg1.row + peg2.row)/2;
-                    //     iCol = (peg1.col + peg2.col)/2;
-                    // }
-                    if ((Math.abs(peg1.row - peg2.row) === 2 ||
-                        (peg1.row - peg2.row) === 0) &&
-                        (Math.abs(peg1.col - peg2.col) === 2 ||
-                            (peg1.col - peg2.col) === 0)){
-                        iRow = (peg1.row + peg2.row)/2;
-                        iCol = (peg1.col + peg2.col)/2;
-                    }
-                    if (iRow >= 0 && iCol >= 0){
-                        //Check if intermediate peg is filled
-                        var iPegIndex = (iRow*(iRow + 1)/2) + iCol;
-                        if (!this.pegs[iPegIndex].isEmpty){
-                            //Make the valid move
-                            this.pegs[nP1].isEmpty = true;
-                            this.pegs[nP2].isEmpty = false;
-                            this.pegs[iPegIndex].isEmpty = true;
-                        } else{
-                            result = "err: Invalid move";
-                            break;
-                        }
-                    } else {
-                        result = "err: Invalid move";
-                        break;
+    make_a_move: function(){
+        var result = {movedFrom:'', movedTo:'', removed:'', status:'', msg:''};
+        if (this.moveFrom >= 0 && this.moveTo >= 0){
+            var nP1 = this.moveFrom;
+            var nP2 = this.moveTo;
+            var peg1 = this.pegs[nP1];
+            var peg2 = this.pegs[nP2];
+            result.movedFrom = peg1.id;
+            result.movedTo = peg2.id;
+            if (!peg1.isEmpty && peg2.isEmpty){
+                //Check if a valid move can be made
+                var iRow = -1;
+                var iCol = -1;
+                if ((Math.abs(peg1.row - peg2.row) === 2 ||
+                    (peg1.row - peg2.row) === 0) &&
+                    (Math.abs(peg1.col - peg2.col) === 2 ||
+                        (peg1.col - peg2.col) === 0)){
+                    iRow = (peg1.row + peg2.row)/2;
+                    iCol = (peg1.col + peg2.col)/2;
+                }
+                if (iRow >= 0 && iCol >= 0){
+                    //Check if intermediate peg is filled
+                    var iPegIndex = (iRow*(iRow + 1)/2) + iCol;
+                    if (!this.pegs[iPegIndex].isEmpty){
+                        //Make the valid move
+                        this.pegs[nP1].isEmpty = true;
+                        this.pegs[nP2].isEmpty = false;
+                        this.pegs[iPegIndex].isEmpty = true;
+                        result.removed = this.pegs[iPegIndex].id;
+                        result.status = "success"
+                        result.msg = result.movedFrom + " - " +
+                                     result.movedTo + "; removed " +
+                                     result.removed;
+                    } else{
+                        result.status = "error";
+                        result.msg = "Invalid move"
                     }
                 } else {
-                    result = "err: Invalid move";
-                    break;
+                    result.status = "error";
+                    result.msg = "Invalid move"
                 }
-            }
+              } else {
+                  result.status = "error";
+                  result.msg = "Invalid move"
+              }
+        } else{
+            result.status = "error";
+            result.msg = "Nothing selected."
         }
         return result;
     },
@@ -158,6 +162,8 @@ Board.prototype = {
 
 }
 
+var myBoard = new Board();
+
 $(document).ready( function(){
     //Toggle rules when rules heading is clicked
     $("#rules").on('click', function(){
@@ -166,23 +172,61 @@ $(document).ready( function(){
     });
 
     $("#new-game").on('click', function(){
-        var myBoard = new Board();
         $("body").append(myBoard.html);
-        var label = $("<p></p>").text("Make a move: ");
-        var txtBox = $("<input></input>");
-        txtBox.attr("type","text");
-        txtBox.attr("name","moves-text");
-        txtBox.attr("id","moves-text")
-        var submitBtn = $("<button></button>");
-        submitBtn.attr("class","btn btn-primary");
-        submitBtn.attr("id", "submit-btn");
-        submitBtn.attr("type", "button");
-        submitBtn.text("Submit");
-        label.append(txtBox,submitBtn);
-        submitBtn.on('click', function(){
-            var thisMove = $("#moves-text").val();
-            alert(thisMove);
+
+        $("circle").on('click', function(){
+            var curId = $(this).attr("id");
+            var curIndex = $(this).attr("index");
+            // alert('Circle with index = ' + curIndex " was clicked.");
+            if (myBoard.moveFrom < 0){
+                myBoard.moveFrom = curIndex;
+            } else if (myBoard.moveTo < 0){
+                myBoard.moveTo = curIndex;
+                var res = myBoard.make_a_move();
+                // alert(res.msg);
+                if (res.status === "success"){
+                  var mFrom = "#" + res.movedFrom;
+                  var mTo = "#" + res.movedTo;
+                  var remv = "#" + res.removed;
+                  $(mFrom).attr("state", "empty");
+                  $(mTo).attr("state", "occupied");
+                  $(remv).attr("state", "empty");
+                  var newMove = $("<span></span>");
+                  newMove.text(res.msg);
+                  $("#move-log").append($("<br />"));
+                  $("#move-log").append(newMove);
+                }
+                myBoard.moveFrom = -1;
+                myBoard.moveTo = -1;
+            }
         });
-        $("#game-board").append(label);
+        $("text").on('click', function(){
+            var curId = $(this).attr("id");
+            var curIndex = $(this).attr("index");
+            // alert('Text with id = ' + curId + " was clicked.");
+            if (myBoard.moveFrom < 0){
+                myBoard.moveFrom = curIndex;
+            } else if (myBoard.moveTo < 0){
+                myBoard.moveTo = curIndex;
+                var res = myBoard.make_a_move();
+                // alert(res.msg);
+                if (res.status === "success"){
+
+                  var mFrom = "#" + res.movedFrom;
+                  var mTo = "#" + res.movedTo;
+                  var remv = "#" + res.removed;
+                  $(mFrom).attr("state", "empty");
+                  $(mTo).attr("state", "occupied");
+                  $(remv).attr("state", "empty");
+                  var newMove = $("<span></span>");
+                  newMove.text(res.msg);
+                  $("#move-log").append($("<br />"));
+                  $("#move-log").append(newMove);
+                }
+                myBoard.moveFrom = -1;
+                myBoard.moveTo = -1;
+            }
+        });
+
     });
 });
